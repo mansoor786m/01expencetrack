4567c21041dad5f45b009aaef6be490b0782c6fd
Muhammad Mansoor <mansoorjbd@gmail.com>
2024-07-15 07:40:34 +0500

    updated

--- 01expencetrack/src/App.js
+++ 01expencetrack/src/App.js
@@ -1,11 +1,30 @@
+import React from 'react';
 
+// Import CSS
+import './App.css';
+
+// Import Components
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { AccountSummary } from './components/AccountSummary';
+import { TransactionHistory } from './components/TransactionHistory';
+import { AddTransaction } from './components/AddTransaction';
+
+// Import Provider
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hellow</h1>
-      <h2>my file</h2>
-    </div>
+    <h1>hellow</h1>
+    // <GlobalProvider>
+    //   <Header />
+    //   <div className="container">
+    //     <Balance />
+    //     <AccountSummary />
+    //     <TransactionHistory />
+    //     <AddTransaction />
+    //   </div>
+    // </GlobalProvider>
   );
 }
 
